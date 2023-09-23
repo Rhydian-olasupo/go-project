18e42d8d441809eb828c06e3479b03bac1a33cc8
Olasupo <alfredolasupo@gmail.com>
2023-09-23 04:14:02 +0100

    working with slices

--- Array_slices/array.go
+++ Array_slices/array.go
@@ -1,9 +1,17 @@
 package main
 
-func Sum(numbers [5]int) (sum_value int) {
+func Sum(numbers []int) (sum_value int) {
 
 	for _, j := range numbers {
 		sum_value += j
 	}
 	return
+}
+
+func SumAll(myslices1, myslices2 []int) []int {
+	x := Sum(myslices1)
+	y := Sum(myslices2)
+
+	return []int{x, y}
+
 }
--- Array_slices/sum_test.go
+++ Array_slices/sum_test.go
@@ -1,13 +1,38 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	got := Sum(numbers)
-	want := 15
+	want := 45
 
 	if got != want {
 		t.Errorf("got %v want %v, given %v ", got, want, numbers)
 	}
+}
+
+func TestSlice(t *testing.T) {
+	t.Run("collection of any size (slices)", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4}
+		got := Sum(numbers)
+		want := 10
+
+		if got != want {
+			t.Errorf("got %v want %v , given %v", got, want, numbers)
+		}
+	})
+
+	t.Run("Summation of different slices", func(t *testing.T) {
+		myslices1, myslices2 := []int{1, 2, 3}, []int{1, 2, 3, 4}
+		got := SumAll(myslices1, myslices2)
+		want := []int{6, 10}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v , given %v and %v", got, want, myslices1, myslices2)
+		}
+	})
 }
