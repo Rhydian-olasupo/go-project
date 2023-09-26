06d36e597c2598cb512d1a8bbd88d226d18b652c
Olasupo <alfredolasupo@gmail.com>
2023-09-26 17:24:29 +0100

    structs, methods,interface refactored

--- structs/perimeter.go
+++ structs/perimeter.go
@@ -1,14 +1,32 @@
 package main
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
+type Circle struct {
+	radius float64
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Height * rectangle.Width
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
 }
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+type Shape interface {
+	Area() float64
+}
+
+/*func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}*/
+
+/*func Area(rectangle Rectangle) float64 {
+	return rectangle.Height * rectangle.Width
+}*/
